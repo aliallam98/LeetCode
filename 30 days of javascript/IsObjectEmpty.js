f2693d276f578153858a3eaf6f0fca89d96a9352
aliallam98 <allam98.ali@gmail.com>
2024-09-30 13:30:26 +0300

    .

---
+++ 30 days of javascript/IsObjectEmpty.js
@@ -0,0 +1,30 @@
+/*
+Given an object or an array, return if it is empty.
+
+An empty object contains no key-value pairs.
+An empty array contains no elements.
+You may assume the object or array is the output of JSON.parse.
+
+
+Example 1:
+
+Input: obj = {"x": 5, "y": 42}
+Output: false
+Explanation: The object has 2 key-value pairs so it is not empty.
+Example 2:
+
+Input: obj = {}
+Output: true
+Explanation: The object doesn't have any key-value pairs so it is empty.
+
+*/
+
+// Solution
+
+// const isEmpty = (obj) => {
+//     return !Object.values(obj).length ? true : false;
+//     return !Object.keys(obj).length ? true : false;
+//     // console.log(JSON.stringify(obj).length)
+
+// }
+
