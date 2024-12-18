3d232a6ac430533f36bd98b15c0b184355b33e44
aliallam98 <allam98.ali@gmail.com>
2024-12-18 12:46:24 +0200

    resolved 2 new question

---
+++ 30 days of javascript/functionComposition.js
@@ -0,0 +1,29 @@
+/**
+ * @param {Function[]} functions
+ * @return {Function}
+ */
+
+// var compose = function (functions) {
+//     return function (i) {
+
+//         return functions.reverse().reduce((i,current)=>  {
+//             return current(i)
+//         },i)
+//     };
+// };
+var compose = function (functions) {
+  return function (x) {
+    for (let i = functions.length - 1; i >= 0; i--) {
+      x = functions[i](x);
+    }
+    return x;
+  };
+};
+
+/**
+ * const fn = compose([x => x + 1, x => 2 * x])
+ * fn(4) // 9
+ */
+
+const fn = compose([(x) => x + 1, (x) => x * x, (x) => 2 * x]); //
+console.log(fn(4));
