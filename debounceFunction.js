14174c54e73ac6babf3f4d51616215a396c12d7b
aliallam98 <allam98.ali@gmail.com>
2025-01-27 12:13:20 +0200

    Practiced on debounce

--- debounceFunction.js
+++ debounceFunction.js
@@ -1,46 +1,20 @@
-// const debounce = (func, delay = 500) => {
-//   let timer;
-//   return (...args) => {
-//     clearTimeout(timer);
-//     timer = setTimeout(() => {
-//       func(...args);
-//     }, delay);
-//   };
-// };
-
-const debounce = (func, delay = 500) => {
+function debounce(func, timeout = 300) {
   let timer;
   return (...args) => {
     clearTimeout(timer);
     timer = setTimeout(() => {
       func.apply(this, args);
-    }, delay);
+    }, timeout);
   };
-};
+}
+function logMessage() {
+  console.log("Hello Ali");
+}
+const debounceLog = debounce(() => logMessage());
 
-const sumLog = (...rest) => {
-  const results = rest.reduce((acc, current) => acc + current);
-  console.log(results);
-};
-
-debounce(sumLog(10, 20, 30));
-debounce(sumLog(10, 20, 30));
-debounce(sumLog(10, 20, 30));
-debounce(sumLog(10, 20, 30));
-debounce(sumLog(10, 20, 30));
-debounce(sumLog(10, 20, 30));
-debounce(sumLog(10, 20, 30));
-debounce(sumLog(10, 20, 30));
-debounce(sumLog(10, 20, 30));
-debounce(sumLog(10, 20, 30));
-debounce(sumLog(10, 20, 30));
-debounce(sumLog(10, 20, 30));
-debounce(sumLog(10, 20, 30));
-debounce(sumLog(10, 20, 30));
-debounce(sumLog(10, 20, 30));
-debounce(sumLog(10, 20, 30));
-debounce(sumLog(10, 20, 30));
-debounce(sumLog(10, 20, 30));
-debounce(sumLog(10, 20, 30));
-debounce(sumLog(10, 20, 30));
-debounce(sumLog(10, 20, 30));
+debounceLog();
+debounceLog();
+debounceLog();
+debounceLog();
+debounceLog();
+debounceLog();
